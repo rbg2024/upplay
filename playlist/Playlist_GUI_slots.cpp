7370d048bf2895b9be42aa5b98f047f8bba339b1
Jean-Francois Dockes <jf@dockes.org>
2014-09-09 15:50:14 +0200

    astyle

--- playlist/Playlist_GUI_slots.cpp
+++ playlist/Playlist_GUI_slots.cpp
@@ -45,8 +45,9 @@ void Playlist::psl_play()
         return;
     }
 
-    if(_v_meta_data.size() == 0) 
+    if (_v_meta_data.size() == 0) {
         return;
+    }
 
     // state was stop until now
     if (_cur_play_idx < 0) {
@@ -54,7 +55,7 @@ void Playlist::psl_play()
         int track_num = 0;
         MetaData md = _v_meta_data[track_num];
 
-        if(checkTrack(md)){
+        if (checkTrack(md)) {
             send_cur_playing_signal(track_num);
         }
     } else {
@@ -84,8 +85,9 @@ void Playlist::psl_forward()
 // GUI -->
 void Playlist::psl_backward()
 {
-    if (_cur_play_idx <= 0) 
+    if (_cur_play_idx <= 0) {
         return;
+    }
 
     int track_num = _cur_play_idx - 1;
     MetaData md = _v_meta_data[track_num];
@@ -99,8 +101,9 @@ void Playlist::psl_backward()
 // GUI -->
 void Playlist::psl_change_track(int new_row)
 {
-    if(uint(new_row) >= _v_meta_data.size()) 
+    if (uint(new_row) >= _v_meta_data.size()) {
         return;
+    }
 
     for (uint i = 0; i < _v_meta_data.size(); i++) {
         _v_meta_data[i].pl_playing = (new_row == (int)i);
@@ -109,7 +112,7 @@ void Playlist::psl_change_track(int new_row)
 
     MetaData md = _v_meta_data[new_row];
     if (checkTrack(md)) {
-       send_cur_playing_signal(new_row);
+        send_cur_playing_signal(new_row);
     } else {
         _cur_play_idx = -1;
         _v_meta_data.setCurPlayTrack(_cur_play_idx);
@@ -120,21 +123,24 @@ void Playlist::psl_change_track(int new_row)
 }
 
 // insert tracks (also drag & drop)
-void Playlist::psl_insert_tracks(const MetaDataList& v_metadata, int row){
+void Playlist::psl_insert_tracks(const MetaDataList& v_metadata, int row)
+{
 
     //bool instant_play = ((_v_meta_data.size() == 0) && (!_is_playing));
 
     // possibly the current playing index has to be updated
-    if(row < _cur_play_idx && _cur_play_idx != -1)
+    if (row < _cur_play_idx && _cur_play_idx != -1) {
         _cur_play_idx += v_metadata.size();
+    }
 
     // insert new tracks
     for (uint i = 0; i < v_metadata.size(); i++) {
 
         MetaData md = v_metadata[i];
         _v_meta_data.insert_mid(md, i + row + 1);
-        if (md.pl_playing) 
+        if (md.pl_playing) {
             _cur_play_idx = (i + row + 1);
+        }
     }
 
 
@@ -144,16 +150,17 @@ void Playlist::psl_insert_tracks(const MetaDataList& v_metadata, int row){
 
 void Playlist::psl_append_tracks(MetaDataList& v_md)
 {
-    if (v_md.size() == 0)
+    if (v_md.size() == 0) {
         return;
+    }
 
     int initsize = int(_v_meta_data.size());
 
-    foreach (MetaData md, v_md)
-        _v_meta_data.push_back(md);
+    foreach(MetaData md, v_md)
+    _v_meta_data.push_back(md);
 
     if (_cur_play_idx == initsize - 1) {
-        // We were playing the last track. Set new next track for gapless, 
+        // We were playing the last track. Set new next track for gapless,
         send_next_playing_signal(_cur_play_idx + 1);
     }
     emit sig_playlist_created(_v_meta_data, _cur_play_idx, 0);
@@ -169,25 +176,28 @@ void Playlist::remove_row(int row)
     psl_remove_rows(remove_list);
 }
 
-void Playlist::psl_remove_rows(const QList<int> & rows, bool select_next_row)
+void Playlist::psl_remove_rows(const QList<int>& rows, bool select_next_row)
 {
-    if(rows.size() == 0) 
+    if (rows.size() == 0) {
         return;
+    }
 
     MetaDataList v_tmp_md;
 
     int n_tracks = (int) _v_meta_data.size();
     int n_tracks_before_cur_idx = 0;
 
-    if(rows.contains(_cur_play_idx)) 
+    if (rows.contains(_cur_play_idx)) {
         _cur_play_idx = -1;
+    }
 
     int first_row = rows[0];
     for (int i = 0; i < n_tracks; i++) {
 
         if (rows.contains(i)) {
-            if (i < _cur_play_idx)
+            if (i < _cur_play_idx) {
                 n_tracks_before_cur_idx++;
+            }
             continue;
         }
 
@@ -202,15 +212,17 @@ void Playlist::psl_remove_rows(const QList<int> & rows, bool select_next_row)
 
     _cur_play_idx -= n_tracks_before_cur_idx;
 
-    if(_cur_play_idx < 0 || _cur_play_idx >= (int) v_tmp_md.size() )
+    if (_cur_play_idx < 0 || _cur_play_idx >= (int) v_tmp_md.size()) {
         _cur_play_idx = -1;
-    else
+    } else {
         v_tmp_md[_cur_play_idx].pl_playing = true;
+    }
 
     _v_meta_data = v_tmp_md;
 
-    if(select_next_row)
+    if (select_next_row) {
         _v_meta_data[first_row].pl_selected = true;
+    }
 
     psl_save_playlist_to_storage();
 
