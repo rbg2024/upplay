05c4f3f3147a692bcbe38df05272c3e0e961f504
Jean-Francois Dockes <jf@dockes.org>
2014-09-10 07:25:50 +0200

    astyle

--- GUI/playlist/GUI_Playlist.h
+++ GUI/playlist/GUI_Playlist.h
@@ -50,82 +50,81 @@
 
 class GUI_InfoDialog;
 
-	class GUI_Playlist : public QWidget, private Ui::Playlist_Window
-	{
-		Q_OBJECT
+class GUI_Playlist : public QWidget, private Ui::Playlist_Window {
+    Q_OBJECT
 
-		public:
-			GUI_Playlist(QWidget *parent, GUI_InfoDialog* dialog);
-			~GUI_Playlist();
+public:
+    GUI_Playlist(QWidget *parent, GUI_InfoDialog* dialog);
+    ~GUI_Playlist();
 
-            void dragEnterEvent(QDragEnterEvent* event);
-            void dragLeaveEvent(QDragLeaveEvent* event);
-            void dropEvent(QDropEvent* event);
-            void dragMoveEvent(QDragMoveEvent* event);
-
-
-		signals:
-			void selected_row_changed(int);
-			void clear_playlist();
-			void save_playlist(const QString&);
-			void playlist_mode_changed(const Playlist_Mode&);
-
-            void dropped_tracks(const MetaDataList&, int);
-            void sig_rows_removed(const QList<int>&, bool);
-
-			void search_similar_artists(const QString&);
-            void sig_no_focus();
+    void dragEnterEvent(QDragEnterEvent* event);
+    void dragLeaveEvent(QDragLeaveEvent* event);
+    void dropEvent(QDropEvent* event);
+    void dragMoveEvent(QDragMoveEvent* event);
 
 
+signals:
+    void selected_row_changed(int);
+    void clear_playlist();
+    void save_playlist(const QString&);
+    void playlist_mode_changed(const Playlist_Mode&);
 
-		public slots:
-            void fillPlaylist(MetaDataList&, int, int);
-			void track_changed(int);
+    void dropped_tracks(const MetaDataList&, int);
+    void sig_rows_removed(const QList<int>&, bool);
 
-            void library_path_changed(QString);
-			void set_radio_active(int radio);
-			void psl_show_small_playlist_items(bool small_items);
-            void language_changed();
+    void search_similar_artists(const QString&);
+    void sig_no_focus();
 
 
 
-		private slots:
+public slots:
+    void fillPlaylist(MetaDataList&, int, int);
+    void track_changed(int);
 
-            void selection_changed(MetaDataList&);
-            void double_clicked(int);
-            void clear_playlist_slot();
-			void playlist_mode_changed_slot();
-
-            void psl_info_tracks();
-            void psl_edit_tracks();
-
-            void btn_numbers_changed(bool);
-            void metadata_dropped(const MetaDataList&, int);
-            void rows_removed(const QList<int>&, bool select_next_row);
-            void no_focus();
+    void library_path_changed(QString);
+    void set_radio_active(int radio);
+    void psl_show_small_playlist_items(bool small_items);
+    void language_changed();
 
 
-		private:
 
-			Ui::Playlist_Window* 			ui;
-			QWidget*						_parent;
-			GUI_InfoDialog*					_info_dialog;
+private slots:
 
-			Playlist_Mode					_playlist_mode;
+    void selection_changed(MetaDataList&);
+    void double_clicked(int);
+    void clear_playlist_slot();
+    void playlist_mode_changed_slot();
 
-			int			_radio_active;
-            qint64      _total_msecs;
+    void psl_info_tracks();
+    void psl_edit_tracks();
+
+    void btn_numbers_changed(bool);
+    void metadata_dropped(const MetaDataList&, int);
+    void rows_removed(const QList<int>&, bool select_next_row);
+    void no_focus();
 
 
-			void initGUI();
+private:
 
-            void set_total_time_label();
-			void check_dynamic_play_button();
+    Ui::Playlist_Window*            ui;
+    QWidget*                        _parent;
+    GUI_InfoDialog*                 _info_dialog;
 
-    protected:
-            void changeEvent(QEvent* e);
-            void resizeEvent(QResizeEvent *e);
-            void focusInEvent(QFocusEvent *e);
+    Playlist_Mode                   _playlist_mode;
+
+    int         _radio_active;
+    qint64      _total_msecs;
+
+
+    void initGUI();
+
+    void set_total_time_label();
+    void check_dynamic_play_button();
+
+protected:
+    void changeEvent(QEvent* e);
+    void resizeEvent(QResizeEvent *e);
+    void focusInEvent(QFocusEvent *e);
 
 
 };
